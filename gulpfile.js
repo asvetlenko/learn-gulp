ff0f2b9046334531e722c26cbfb026fa49c3c3e2
alexey <asvetlenko@gmail.com>
2016-07-01 00:08:24 +0300

    gulp exersise 4 copy styles with set "{base: 'frontend'}" obviously

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,12 @@
 'use strict';
 
 const gulp = require('gulp');
+const styles = require('gulp-stylus');
 
-gulp.task('default', function () {
-    return gulp.src('./source/**/*.{css,log}') //minimatch
-        .on('data', function (file) {
-            var logObjBase = {
-                contents: file.contents,
-                path: file.path,
-                cwd: file.cwd,
-                base: file.base
-            };
-            console.log('----- base:', logObjBase);
-
-            var logObj2 = {
-                relative: file.relative,
-                dirname: file.dirname, // ../source/client.js
-                basename: file.basename, // client.js
-                stem: file.stem, // client
-                extname: file.extname // .js
-            };
-
-            console.log('-----path component helpers:', logObj2); // js/client.js
-        })
-        //.pipe(gulp.dest('result-folder'));
-        .pipe(gulp.dest(function (file) {
-            var resultPath = '';
-            switch (file.extname){
-                case '.js' :{
-                    resultPath = 'appJS';
-                    break;
-                }
-                case '.css' :{
-                    resultPath = 'appStyle';
-                    break;
-                }
-                default:{
-                    resultPath = 'dest';
-                    break;
-                }
-            }
-
-            return resultPath;
-        }));
+gulp.task('build:styles', () => {
+    return gulp.src('frontend/styles/**/*.styl', {base: 'frontend'})
+        .pipe(styles())
+        .pipe(gulp.dest('public')); //7:05:
 });
+
+gulp.task('default', gulp.series('build:styles'));
