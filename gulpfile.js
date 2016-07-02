9003f155ce738316e2710fe54a4690bc9d69371b
alexey <asvetlenko@gmail.com>
2016-07-02 22:27:14 +0300

    gulp exersise 4 add debug

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const gulp = require('gulp');
-const styles = require('gulp-stylus');
+const stylus = require('gulp-stylus');
+const concat = require('gulp-concat');
+const debug = require('gulp-debug');
 
 gulp.task('build:styles', () => {
     return gulp.src('frontend/styles/**/*.styl', {base: 'frontend'})
-        .pipe(styles())
+        .pipe(debug({title: 'src'}))
+        .pipe(stylus())
+        .pipe(debug({title: 'stylus'}))
+        .pipe(concat('all.css'))
+        .pipe(debug({title: 'concat'}))
         .pipe(gulp.dest('public')); //7:05:
 });
 
