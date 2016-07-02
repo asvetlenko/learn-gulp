fdc8aa40371a586e7093f419746186b1fcb2d61a
alexey <asvetlenko@gmail.com>
2016-07-02 22:45:29 +0300

    gulp exersise 4 add source maps

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,20 @@ const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const concat = require('gulp-concat');
 const debug = require('gulp-debug');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build:styles', () => {
-    return gulp.src('frontend/styles/**/*.styl', {base: 'frontend'})
+    return gulp.src('frontend/styles/main.styl', {base: 'frontend'})
+        .pipe(debug({title: 'sourcemaps.init'}))
+        .pipe(sourcemaps.init())
         .pipe(debug({title: 'src'}))
         .pipe(stylus())
         .pipe(debug({title: 'stylus'}))
         .pipe(concat('all.css'))
         .pipe(debug({title: 'concat'}))
-        .pipe(gulp.dest('public')); //7:05:
+        .pipe(sourcemaps.write('.'))
+        .pipe(debug({title: 'sourcemaps.write'}))
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('default', gulp.series('build:styles'));
