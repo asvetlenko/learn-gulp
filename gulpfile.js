118e76295a23d2fb8b3ef1186f7748b8ae5044bc
alexey <asvetlenko@gmail.com>
2016-07-17 12:50:47 +0300

    gulp exersise 6 gulp-remember

--- gulpfile.js
+++ gulpfile.js
@@ -8,37 +8,25 @@ const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const del = require('del');
 const newer = require('gulp-newer');
+const remember = require('gulp-remember');
+const autoprefixer = require('gulp-autoprefixer');
+const path = require('path');
 
-const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
-gulp.task('build:styles', () => {
-    return gulp.src('frontend/styles/main.styl', {base: 'frontend'})
-        .pipe(gulpIf(isDev, sourcemaps.init()))
+gulp.task('styles', () => {
+    return gulp.src('frontend/styles/**/*.styl', {base: 'frontend', since: gulp.lastRun('styles')})
         .pipe(stylus())
+        .pipe(autoprefixer())
+        .pipe(remember('styles'))
         .pipe(concat('all.css'))
-        .pipe(gulpIf(isDev, sourcemaps.write('.')))
         .pipe(gulp.dest('public'));
 });
-
-gulp.task('build:clean', function () {
-    return del('public');
-});
-gulp.task('build:assets', function () {
-    return gulp.src('frontend/assets/**', {base: 'frontend', since: gulp.lastRun('build:assets')})
-        .pipe(newer('public'))
-        .pipe(debug({title:'assets'}))
-        .pipe(gulp.dest('public'));
-});
-
-gulp.task('default', gulp.series('build:clean',
-    gulp.parallel('build:styles', 'build:assets')));
-
 
 gulp.task('watch', function (callback) {
-    gulp.watch('frontend/styles/**/*.*', gulp.series('build:styles'));
-    gulp.watch('frontend/assets/**/*.*', gulp.series('build:assets'));
+    gulp.watch('frontend/styles/**/*.styl', gulp.series('styles')).on('unlink', function (filepath) {
+        remember.forget('styles', path.resolve(filepath));
+    });
 
     callback();
 });
 
-gulp.task('dev', gulp.series('default', 'watch'));
+gulp.task('dev', gulp.series('styles', 'watch'));
